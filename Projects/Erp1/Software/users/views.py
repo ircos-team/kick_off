1de0f6c45c1fefe03e01f736e5f11a33d16273a8
toufik <djebab.toufik@gmail.com>
2021-01-14 17:05:28 +0100

    first erpome project commit

---
+++ Projects/Erp1/Software/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    print("search called")
+ 
+    return render(request,'users/home.html')
